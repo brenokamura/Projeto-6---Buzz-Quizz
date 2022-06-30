86d6065b9b3efe4467bc82c23968775fd86db5b4
Kássia <kassia016@gmail.com>
2022-06-29 21:26:59 -0300

    html e css da tela3 em andamento

--- script.js
+++ script.js
@@ -8,7 +8,35 @@ function obterQuizzes() {
     promise.catch(() => alert("Erro coleta de dados API"))
 }
 
+// function renderizarQuizzes() {
 
-function renderizarQuizzes (){
-    
-}
+//     let tela1 = document.querySelector(".telas");
+//     tela1.innerHTML = ` 
+//     <div class="tela-um">
+//             <header class="topo">BuzzQuizz</header>
+//             <div class="conteudo">
+//                 <div class="quizzes-do-usuario">
+//                     <p>Você não criou nenhum <br> quizz ainda :(</p>
+//                     <button class="criar-quizz" onclick="criarQuizz()">Criar Quizz</button>
+//                 </div>
+//             </div>
+
+//             <div class="todos-quizzes">
+//                 <div class="titulo-todos-quizzes">Todos os Quizzes</div>
+//                 <div class="container-todos-quizzes">
+                    
+//                 </div>
+//             </div>
+//         </div>
+//      `
+//     let todosQuizzes = document.querySelector(".todos-quizzes");
+//     todosQuizzes.innerHTML 
+
+
+    // <div class="quizz" onclick="telaDois(this)">
+    //     <img src="erased-thumb.jpg" class="imagem-quizz">
+    //         <div class="nome-quizz">O quanto você conhece Erased?</div>
+    // </div>
+
+// }
+
